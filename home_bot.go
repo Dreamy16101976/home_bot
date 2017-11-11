7bd5a450bd5bfaef952888bc053300f60f986d3c
Alexey V. Voronin @ FoxyLab <Dreamy16101976@users.noreply.github.com>
2017-11-11 15:11:20 +0300

    Add files via upload

--- home_bot.go
+++ home_bot.go
@@ -17,7 +17,8 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 
-    CREDITS
+	CREDITS
+	Syfaro - Golang bindings for the Telegram Bot API https://github.com/go-telegram-bot-api/telegram-bot-api
     Brian C. Lane - digitemp https://www.digitemp.com/
     Ted Burke - RobotEyez https://github.com/tedburke/RobotEyez
 */
